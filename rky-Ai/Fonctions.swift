934488eec895e92141eb4b9bdae4bcc4af0a003f
e-coucou <eric@ecoucou.com>
2016-07-20 23:15:09 +0200

    first commit

---
+++ rky-Ai/Fonctions.swift
@@ -0,0 +1,18 @@
+//
+//  Fonctions.swift
+//  rky-Ai
+//
+//  Created by Eric PLAIDY on 16/07/2016.
+//  Copyright © 2016 eCoucou. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import MapKit
+
+// Map variables
+var loc_longitude:CLLocationDegrees = 2.308216
+var loc_latitude:CLLocationDegrees = 48.8338031
+var initialLocation = CLLocation(latitude: loc_latitude, longitude: loc_longitude)
+var regionRadius: CLLocationDistance = 1000
+var l_zone: CLLocationDistance = 420
--- rky-Ai/ViewController.swift
+++ rky-Ai/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var maps: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        maps.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
+        centerMapOnLocation(initialLocation)
+        var cir_zone:MKCircle = MKCircle(centerCoordinate: CLLocationCoordinate2D(latitude: loc_latitude, longitude:loc_longitude), radius: l_zone)
+        maps.addOverlay(cir_zone)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        maps.setRegion(coordinateRegion, animated: true)
+    }
+
 
 }
+// VC extension
+//
+extension ViewController: MKMapViewDelegate {
+ 
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let overlayRenderer = MKCircleRenderer(overlay: overlay);
+        overlayRenderer.lineWidth = 1.0
+        overlayRenderer.strokeColor = UIColor.blueColor()
+        overlayRenderer.fillColor = UIColor.blueColor().colorWithAlphaComponent(0.1)
+        return overlayRenderer
+    }
 
+}
